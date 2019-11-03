4b9daf1ea35b4cf25f0cee905caac8603ed946c8
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-11-02 22:19:04 -0700

    First commit

---
+++ DistributedInformationFilter/DistributedInformationFilter.m
@@ -0,0 +1,72 @@
+classdef DistributedInformationFilter < handle
+    properties (SetAccess = protected)
+        state_vector            % x(k)
+        state_covmat            % P(k)
+        info_vector             % z(k)
+        info_matrix             % Z(k)
+        obs_matrix              % H(k)
+        obs_covmat              % R(k)
+        obs_info_vector         % i(k)
+        obs_info_vector_prev    % i(k-1)
+        obs_info_vector_joint   % u(k)
+        obs_info_matrix         % I(k)
+        obs_info_matrix_prev    % I(k-1)
+        obs_info_matrix_joint   % U(k)
+        out_info_vector_pool    % 
+        out_info_matrix_pool    %
+        obs_mesured             % y(k)
+        Ad
+    end
+    methods
+        function obj = DistributedInformationFilter(args)
+            
+        end
+        function propagateCovarianceMatrix(this)
+            Ad = this.Ad;
+            % TODO: Add control and disturbance noises
+            this.state_covmat = Ad*this.state_covmat*Ad.';
+        end
+        function getInformationVector(this)
+        end
+        function calculateObservationInformation(this)
+            H = this.obs_matrix;
+            R = this.obs_covmat;
+            this.obs_info_vector = H.'/R*y;
+            this.obs_info_matrix = H.'/R*H;
+        end
+        function addOutSourceInformationIntoPool(this, info_vector, info_matrix)
+            this.out_info_vector_pool = this.out_info_vector_pool + info_vector;
+            this.out_info_matrix_pool = this.out_info_matrix_pool + info_matrix;
+        end
+        function integrateMultiSourceInformation(this)
+            this.obs_info_vector_joint = ...
+                this.obs_info_vector - this.obs_info_vector_prev ...
+                + this.out_info_vector_pool;
+            this.obs_info_matrix_joint = ...
+                this.obs_info_matrix - this.obs_info_matrix_prev ...
+                + this.out_info_matrix_pool;
+        end
+        function computePosteriorPdf(this)
+            this.info_vector = this.info_vector + N * this.obs_info_vector_joint;
+            this.info_matrix = this.info_matrix + N * this.obs_info_matrix_joint;
+            this.state_covmat = inv(this.info_matrix);
+            this.state_vector = this.state_covmat + this.info_vector;
+
+        end
+        function setMeasurementData(this, obs_measured)
+            this.obs_measured  = obs_measured;
+        end
+        function setObservationMatrix(this, pos_i, pos_j, agent_id_i, agent_id_j, obs_index)
+            dist = norm(pos_i - pos_j);
+            DIM  = numel(pos_i);
+            for iDim = 1:DIM
+                % Agent ID: i
+                this.obs_matrix(obs_index, 2*DIM*(agent_id_i-1)+iDim)     = (pos_i(iDim,1)-pos_j(iDim,1))/dist;
+                this.obs_matrix(obs_index, 2*DIM*(agent_id_i-1)+DIM+iDim) = 0;
+                % Agent ID: j
+                this.obs_matrix(obs_index, 2*DIM*(agent_id_j-1)+iDim)     = (pos_j(iDim,1)-pos_i(iDim,1))/dist;
+                this.obs_matrix(obs_index, 2*DIM*(agent_id_j-1)+DIM+iDim) = 0;
+            end
+        end
+    end
+end
