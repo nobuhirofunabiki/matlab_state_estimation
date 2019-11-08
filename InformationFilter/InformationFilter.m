132f46928949a37a39800a6fdbdeff7437d1de76
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-11-08 12:13:12 -0800

    Implement the constructor of InformationFilter

--- InformationFilter/InformationFilter.m
+++ InformationFilter/InformationFilter.m
@@ -1,17 +1,25 @@
 classdef InformationFilter < handle
     properties (SetAccess = protected)
-        num_variables       % 
+        num_variables       % Number of variables
+        num_dims            % Number of dimensions (2D or 3D)
+        num_agents          % Number of agents
         state_vector        % x(k): State vector
         state_covmat        % P(k): Covariance matrix of state
         info_vector         % xi(k): Information vector
         info_matrix         % Sigma(k): Information matrix
-        obs_matrix          % H(k): Measurement matrix
-        obs_measures        % z(k): Measurement vector
         system_matrix       % Ad(k): System matrix
     end
     methods
         function obj = InformationFilter(args)
-            num_variables = args.num_variables;
+            num_vars            = args.num_variables;
+            obj.num_variables   = num_vars;
+            obj.num_dims        = args.num_dims;
+            obj.num_agents      = args.num_agents;
+            obj.state_vector    = zeros(num_vars, 1);
+            obj.state_covmat    = zeros(num_vars, num_vars);
+            obj.info_vector     = zeros(num_vars, 1);
+            obj.info_matrix     = zeros(num_vars, num_vars);
+            obj.system_matrix   = zeros(num_vars, num_vars);
         end
         function propagateCovarianceMatrix(this)
             Ad = this.system_matrix;
