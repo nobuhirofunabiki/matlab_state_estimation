13ba524a84fb9cf48d54d2547c422f93832762ff
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-11-08 10:00:05 -0800

    First implementation of the centralized information filter

---
+++ InformationFilter/InformationFilter.m
@@ -0,0 +1,38 @@
+classdef InformationFilter < handle
+    properties (SetAccess = protected)
+        num_variables       % 
+        state_vector        % x(k): State vector
+        state_covmat        % P(k): Covariance matrix of state
+        info_vector         % xi(k): Information vector
+        info_matrix         % Sigma(k): Information matrix
+        obs_matrix          % H(k): Measurement matrix
+        obs_measures        % z(k): Measurement vector
+        system_matrix       % Ad(k): System matrix
+    end
+    methods
+        function obj = InformationFilter(args)
+            num_variables = args.num_variables;
+        end
+        function propagateCovarianceMatrix(this)
+            Ad = this.system_matrix;
+            % TODO: Add control and disturbance noises
+            this.state_covmat = Ad*this.state_covmat*Ad.';
+        end
+        function addObservationInformation(this, obs_matrix, obs_covmat, obs_measured)
+            H = obs_matrix;
+            R = obs_covmat;
+            y = obs_measured;
+            this.info_vector = this.info_vector + H.'/R*y;
+            this.info_matrix = this.info_matrix + H.'/R*H;
+        end
+        function convertMomentsToInformationForm(this)
+            this.info_matrix = inv(this.state_covmat);
+            this.info_vector =  this.info_matrix * this.state_vector;
+        end
+
+        % Setters
+        function setEstimatedVariable(this, index_begin, index_end, arg_variable)
+            this.state_vector(index_begin:index_end, 1) = arg_variable;
+        end
+    end
+end
