68739bc6b0062b9791382b07c5eb5a8368947c0c
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-12-26 12:17:57 -0800

    [InformationFilter] Add a process noise in the prediction step

--- InformationFilter/InformationFilter.m
+++ InformationFilter/InformationFilter.m
@@ -5,12 +5,12 @@ classdef InformationFilter < handle
         num_agents              % Number of agents
         state_vector            % x(k): State vector
         state_covmat            % P(k): Covariance matrix of state
-        process_noise_covmat    % Q: Covariance matrix of process noise
+        process_noise_covmat    % Q(k): Covariance matrix of process noise
         info_vector             % xi(k): Information vector
         info_matrix             % Sigma(k): Information matrix
         discrete_system_matrix  % Ad(k): Discrete system matrix
     end
-    
+
     methods
         function obj = InformationFilter(args)
             num_vars                    = args.num_variables;
@@ -19,7 +19,7 @@ classdef InformationFilter < handle
             obj.num_agents              = args.num_agents;
             obj.state_vector            = zeros(num_vars, 1);
             obj.state_covmat            = zeros(num_vars, num_vars);
-            obj.process_noise_covmat    = zeros(num_vars, num_vars);
+            obj.process_noise_covmat    = args.process_noise_covmat;
             obj.info_vector             = zeros(num_vars, 1);
             obj.info_matrix             = zeros(num_vars, num_vars);
             obj.discrete_system_matrix  = args.discrete_system_matrix;
@@ -35,15 +35,10 @@ classdef InformationFilter < handle
 
         function predictStateVectorAndCovariance(this)
             Ad = this.discrete_system_matrix;
+            Q = this.process_noise_covmat;
             this.state_vector = Ad*this.state_vector;
-            this.state_covmat = Ad*this.state_covmat*Ad.';
+            this.state_covmat = Ad*this.state_covmat*Ad.' + Q;
         end
-
-        % function propagateCovarianceMatrix(this)
-        %     Ad = this.discrete_system_matrix;
-        %     % TODO: Add control and disturbance noises
-        %     this.state_covmat = Ad*this.state_covmat*Ad.';
-        % end
 
         function addObservationInformation(this, obs_matrix, obs_covmat, measures)
             H = obs_matrix;
