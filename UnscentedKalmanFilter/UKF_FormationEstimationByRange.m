7cb825070bba47209680a31cdf2950dc6077c867
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-12-30 17:01:04 -0800

    [UKF] Add the angle measurements in UKF_FormationEstimationByRange

--- UnscentedKalmanFilter/UKF_FormationEstimationByRange.m
+++ UnscentedKalmanFilter/UKF_FormationEstimationByRange.m
@@ -10,6 +10,7 @@ classdef UKF_FormationEstimationByRange < ...
     end
     properties (SetAccess = private)
         range_sensor_       % Instance of RangeMeasurementInterAgents class
+        angle_sensor_       % Instance of AngleMeasurementMultiAgents class
         position_sensor_    % Instance of PositionMeasurementMultiAgents class
     end
     properties (SetAccess = immutable)
@@ -21,6 +22,7 @@ classdef UKF_FormationEstimationByRange < ...
         function obj = UKF_FormationEstimationByRange(args)
             obj@UKF_LinearDynamics(args);
             obj.range_sensor_           = RangeMeasurementInterAgents(args.rmia);
+            obj.angle_sensor_           = AngleMeasurementMultiAgents(args.angle);
             obj.position_sensor_        = PositionMeasurementMultiAgents(args.pmb);
             obj.num_dimensions          = args.num_dimensions;
             obj.num_agents              = args.num_agents;
@@ -43,25 +45,43 @@ classdef UKF_FormationEstimationByRange < ...
 
     methods (Access = protected)
         function processMeasurements(this, measurements, adjacent_matrix)
-            this.measurements   = vertcat(measurements.ranges, measurements.positions);
+            this.measurements   = vertcat(measurements.ranges, measurements.angles, measurements.positions);
             num_sigma_points    = size(this.sigma_points, 2);
             num_measures        = size(this.measurements, 1);
             num_ranges          = size(measurements.ranges, 1);
+            num_angles          = size(measurements.angles, 1);
             num_positions       = size(measurements.positions, 1);
             z_pred              = zeros(num_measures,1);
             Z                   = zeros(num_measures,num_sigma_points);
             obs_covmat          = zeros(num_measures, num_measures);
-            this.range_sensor_.updateMeasurementCovarianceMatrix(adjacent_matrix);
+            this.range_sensor_.updateMeasurementCovarianceMatrix(adjacent_matrix.range);
+            this.angle_sensor_.setMeasurementCovarianceMatrix(adjacent_matrix.angle);
             obs_covmat(1:num_ranges, 1:num_ranges) ...
                 = this.range_sensor_.getMeasureCovarinaceMatrix();
-            obs_covmat(num_ranges+1:num_measures, num_ranges+1:num_measures) ...
+            obs_covmat(num_ranges+1:num_ranges+num_angles, num_ranges+1:num_ranges+num_angles) ...
+                = this.angle_sensor_.getMeasureCovarinaceMatrix();
+            obs_covmat(num_ranges+num_angles+1:num_measures, num_ranges+num_angles+1:num_measures) ...
                 = this.position_sensor_.getMeasureCovarinaceMatrix();
             for iPoints = 1:num_sigma_points
                 positions = this.getPositionVector(iPoints);
                 this.range_sensor_.calculateMeasurementVectorWithoutNoise(positions);
                 this.position_sensor_.computeMeasurementVector(positions, false);
-                Z(1:num_ranges, iPoints)                 = this.range_sensor_.getMeasurements();
-                Z(num_ranges+1:num_measures, iPoints)    = this.position_sensor_.getMeasurements();
+                this.angle_sensor_.computeMeasurementVector(positions, false);
+                measures_predicted_angle = this.angle_sensor_.getMeasurements();
+                % TODO: How to tuckle the following singular point problem?
+                diff_measures = measures_predicted_angle - measurements.angles;
+                for iMeasures = 1:length(diff_measures)
+                    if (abs(diff_measures(iMeasures,1)) >= pi)
+                        if (measures_predicted_angle(iMeasures,1) > measurements.angles(iMeasures,1))
+                            measures_predicted_angle(iMeasures,1) = measures_predicted_angle(iMeasures,1) - 2*pi;
+                        else
+                            measures_predicted_angle(iMeasures,1) = measures_predicted_angle(iMeasures,1) + 2*pi;
+                        end
+                    end
+                end
+                Z(1:num_ranges, iPoints)                            = this.range_sensor_.getMeasurements();
+                Z(num_ranges+1:num_ranges+num_angles, iPoints)      = measures_predicted_angle;
+                Z(num_ranges+num_angles+1:num_measures, iPoints)    = this.position_sensor_.getMeasurements();
                 z_pred = z_pred + this.weights_mean(1,iPoints)*Z(:,iPoints);
             end
             this.z_pred = z_pred;
