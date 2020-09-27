49c43a0b878e177d58d2be8724bb4f5686ea8f66
Nobuhiro Funabiki <funabiki0819@gmail.com>
2020-09-27 20:03:10 +0900

    First implementation of SR-UKF

---
+++ UnscentedKalmanFilter/SRUKF_TargetTrackingRangeLandmarks.m
@@ -0,0 +1,54 @@
+classdef SRUKF_TargetTrackingRangeLandmarks < SquareRootUnscentedKalmanFilter
+    properties (SetAccess = protected)
+        state_vector
+        state_covmat
+        discrete_system_matrix
+        process_noise_covmat
+        S
+        Sz
+        sqrtQ
+        sqrtR
+    end
+
+    properties (SetAccess = private)
+        range_sensor_       % Class instance of RangeMeasurementLandmarks
+    end
+
+    methods (Access = public)
+        function obj = SRUKF_TargetTrackingRangeLandmarks(args)
+            obj@SquareRootUnscentedKalmanFilter(args);
+            obj.range_sensor_           = RangeMeasurementLandmarks(args.rml);
+            obj.state_vector            = args.state_vector;
+            obj.state_covmat            = args.state_covmat;
+            obj.discrete_system_matrix  = args.discrete_system_matrix;
+            obj.process_noise_covmat    = args.process_noise_covmat;
+            obj.S = chol(args.state_covmat);
+            obj.sqrtQ = chol(args.process_noise_covmat);
+            num_measures = size(obj.range_sensor_.getLandmarkList(), 2);
+            obj.sqrtR = zeros(num_measures, num_measures);
+            obj.Sz = zeros(num_measures, num_measures);
+        end
+    end
+
+    methods (Access = protected)
+        function processMeasurements(this, measurements)
+            this.measurements   = measurements;
+            num_sigma_points    = size(this.sigma_points, 2);
+            num_measures        = size(this.range_sensor_.getLandmarkList(), 2);
+            z_pred              = zeros(num_measures,1);
+            Z                   = zeros(num_measures,num_sigma_points);
+            obs_covmat          = this.range_sensor_.getMeasureCovarinaceMatrix();
+            this.sqrtR          = chol(obs_covmat);
+            for iPoints = 1:num_sigma_points
+                % TODO: only for 2D
+                position = this.sigma_points(1:2, iPoints);
+                this.range_sensor_.computeMeasurementVector(position, false);
+                Z(:,iPoints) = this.range_sensor_.getMeasurements();
+                z_pred = z_pred + this.weights_mean(1,iPoints)*Z(:,iPoints);
+            end
+            this.z_pred = z_pred;
+            this.Z_diff = Z - z_pred(:,ones(1,num_sigma_points));
+            this.Sz = this.qr_decomposition(this.Z_diff, this.sqrtR);
+        end
+    end
+end
---
+++ UnscentedKalmanFilter/SquareRootUnscentedKalmanFilter.m
@@ -0,0 +1,83 @@
+classdef SquareRootUnscentedKalmanFilter < UnscentedKalmanFilterBase
+    properties (Abstract = true, SetAccess = protected)
+        discrete_system_matrix
+        process_noise_covmat
+        S
+        Sz
+        sqrtQ
+        sqrtR
+    end
+
+    methods (Access = public)
+        function obj = SquareRootUnscentedKalmanFilter(args)
+            obj@UnscentedKalmanFilterBase(args);
+        end
+    end
+
+    methods (Access = public)
+        function executeUnscentedKalmanFilter(this, measurements)
+            this.sigma_points = this.generateSigmaPoints();
+            this.predictStates();
+            this.processMeasurements(measurements);
+            this.updateByMeasurements();
+        end
+    end
+
+    methods (Access = protected)
+        function obj = generateSigmaPoints(this)
+            A = this.gamma * this.S;
+            x = this.state_vector;
+            Y = x(:,ones(1,numel(x)));
+            obj = [x Y+A Y-A];
+        end
+
+        function updateByMeasurements(this)
+            this.Pxz = this.X_diff*diag(this.weights_cov)*this.Z_diff';
+            K = (this.Pxz/this.Sz)/(this.Sz).';
+            this.state_vector = this.state_vector + K*(this.measurements - this.z_pred);
+            U = K*(this.Sz).';
+            for i = 1:size(U,2)
+                this.S = cholupdate(this.S, U(:,i), '-');
+            end
+        end
+
+        function predictStates(this)
+            num_sigma_points = size(this.sigma_points, 2);
+            state_vector_prior = zeros(size(this.state_vector));
+            sigma_point_states = zeros(size(this.sigma_points));
+            for iPoints = 1:num_sigma_points
+                sigma_point_states(:,iPoints) = ...
+                    this.discrete_system_matrix*this.sigma_points(:,iPoints);
+                state_vector_prior = state_vector_prior ...
+                    + this.weights_mean(1,iPoints)*sigma_point_states(:,iPoints);
+            end
+            this.state_vector = state_vector_prior;
+            this.X_diff = sigma_point_states - state_vector_prior(:,ones(1,num_sigma_points));
+            this.S = this.qr_decomposition(this.X_diff, this.sqrtQ);
+        end
+    end
+
+    methods (Access = protected)
+        function obj = qr_decomposition(this, x_diff, square_root_covariance)
+            num_sigma_points = size(x_diff,2);
+            num_variables = size(x_diff,1);
+            x_diff_weighted = zeros(num_variables, num_sigma_points-1);
+            for iPoints = 2:num_sigma_points
+                x_diff_weighted(:,iPoints-1) = ...
+                    sqrt(this.weights_cov(1,iPoints))*x_diff(:,iPoints);
+            end
+            M = horzcat(x_diff_weighted, square_root_covariance);
+            [foo, S] = qr(M.');
+            S = S(1:num_variables,1:num_variables);
+            if sign(this.weights_cov(1,1)) == 1
+                operation = '+';
+            elseif sign(this.weights_cov(1,1)) == -1
+                operation = '-';
+            else
+                error('QR decomposition error');
+            end
+            obj = cholupdate(S, sqrt(norm(this.weights_cov(1,1)))*x_diff(:,1), operation);
+        end
+    end
+
+end
